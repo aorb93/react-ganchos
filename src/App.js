6730c553e231947360e87dee6ba4b96c649e39e7
aorb93 <aorb93@gmail.com>
2020-10-19 15:21:10 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component, useState } from "react";
+import "./style.css";
+import Contador from "./Contador";
+
+//Hooks
+
+export default function App() {
+  return (
+    <div>
+      <Contador />
+    </div>
+  );
+}
